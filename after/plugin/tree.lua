1732d846f8c57aebafd1293384ed89e3d1855600
realabdo <abdullah.rm.2020@gmail.com>
2023-10-24 22:12:39 +0300

    showing .ignores in tree

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,3 +1,13 @@
 -- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    filters = {
+        dotfiles = false,
+        custom = { '^.git$' }
+    },
+    git = {
+  enable = true,
+  ignore = false,
+  timeout = 500,
+},
+})
 
